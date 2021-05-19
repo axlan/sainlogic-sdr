6956ac45ea8c53bd0844fcd5d6304737fa60ac71
Jonathan Diamond <feros32@gmail.com>
2021-05-19 13:20:06 -0700

    updated firmware comments

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,33 +6,48 @@
 #include "secrets.h"
 
 // D1 GPIO5
+// Connects to pin that goes low during receive
 #define PIN_IN1 5
 // D2 GPIO4
+// Connects to pin that outputs received data
 #define PIN_IN2 4
 
+// Target time between samples
+// Must be long enough that MCU can keep up with
+// Expected samples/symbol = is SLEEP_US/46.3uS
 #define SLEEP_US 50
 
+// Macros for setting bits
 #define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
 #define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
 
+// Parameters for synching data
 #define MIN_SAMPLES 2
 #define MAX_SAMPLES 10
 
 #define MSG_LEN 128
 
+// Define this to log data packets to serial
+// Instead of normal operation
 //#define DEBUG_SAMPLER
 
+// MQTT Broker to connect to
 const char* mqtt_server = "192.168.1.110";
 
 WiFiClient wifi_client;
 PubSubClient client(wifi_client);
 
+// Class tracking pulse position modulated signal
 class BinaryPpmTracker {
  public:
   BinaryPpmTracker() : last_sample(false) {
     reset();
   }
 
+  // Each sampled is passed in here
+  // The processing is a state machine that looks for the sequences of
+  // high to low, or low to high. The min and max sample parameters are
+  // used to reject pulses that are not part of the actual message.
   void process_sample(bool sample) {
     if (cur_idx == MSG_LEN) {
       return;
@@ -63,16 +78,19 @@ class BinaryPpmTracker {
     last_sample = sample;
   }
 
+  // Clear current state
   void reset() {
     cur_idx = 0;
     edge = 0;
     repetitions = 0;
   }
 
+  // Number of bits received in current burst
   int cur_rx_len() const {
     return cur_idx;
   }
 
+  // Array of bits received in burst
   const uint8_t *get_msg() const {
     return rx_data;
   }
@@ -103,7 +121,8 @@ int last_count = 0;
 unsigned long last_update = 0;
 bool slept = true;
 
-
+// setup WiFi based on ssid and password
+// defined in gitignored secrets.h
 void setup_wifi() {
 
   delay(10);
@@ -128,11 +147,12 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-
+// Don't listen for any MQTT topics
 void callback(char* topic, byte* payload, unsigned int length) {
 
 }
 
+// Reconnect to MQTT Broker
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -153,6 +173,7 @@ void reconnect() {
   }
 }
 
+// Setup pins, serial, Wifi, and MQTT
 void setup() {
   pinMode(PIN_IN1, INPUT);
   pinMode(PIN_IN2, INPUT);
@@ -165,6 +186,7 @@ void setup() {
   client.setKeepAlive(0xFFFF);
 }
 
+// Sample buffer for debugging
 #ifdef DEBUG_SAMPLER
 #define SAMPLE_LEN 10240
 uint8_t samples[SAMPLE_LEN/8];
@@ -177,6 +199,7 @@ void loop() {
     slept = true;
     return;
   }
+  // When debugging, report if unable to keep up with target sample rate
   #ifdef DEBUG_SAMPLER
   if (!slept) {
     Serial.printf("!\n");
@@ -186,6 +209,7 @@ void loop() {
   #endif
   last_update = update;
 
+  // When debugging, log bit sequence following PIN_IN1 going low
   #ifdef DEBUG_SAMPLER
   if (idx ==0 && digitalRead(PIN_IN1)) {
     return;
@@ -209,12 +233,14 @@ void loop() {
   }
   #endif
 
-
+  // Demodulate PPM signal
   tracker.process_sample(digitalRead(PIN_IN2));
   // if (last_count > 0 && tracker.cur_rx_len() == 0) {
   //   Serial.printf("%d\n", last_count);
   // }
   // last_count = tracker.cur_rx_len();
+
+  // If full message is recieved publish it and reset tracker
   if (tracker.cur_rx_len() == MSG_LEN) {
     #ifndef DEBUG_SAMPLER
     for (int i = 0; i < MSG_LEN/8; i++){
