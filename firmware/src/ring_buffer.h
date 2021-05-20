b58eddb4c67e9dbcd12ad99f8a0767ef597c25d9
Jonathan Diamond <feros32@gmail.com>
2021-05-20 14:05:34 -0700

    Add comments

--- firmware/src/ring_buffer.h
+++ firmware/src/ring_buffer.h
@@ -1,3 +1,15 @@
+/**
+ * Bit packed ring buffer for sampling digital value on a pin
+ *
+ * Does not handle overflow or underflow at all.
+ *
+ * Sampler is interrupt safe
+ *
+ * Defining DEBUG_SAMPLER stops the buffer from wrapping allowing for a
+ * one shot sampling from a reset call.
+ */
+
+
 #ifndef RING_BUFFER_H
 #define RING_BUFFER_H
 
@@ -5,17 +17,23 @@
 
 #define SAMPLE_LEN 10240
 
+// The digital been to sample in the sample function
 void set_sample_pin(uint8_t pin);
 
+// Callback for ISR or polling to add samples to buffer
 void ICACHE_RAM_ATTR sample_input();
 
+// How many samples are in buffer
 size_t num_samples();
 
+// Pops the oldest bit off of the buffer
 bool get_next_sample();
 
+// Clears all data from buffer
 void reset_sampler();
 
 #ifdef DEBUG_SAMPLER
+// Directly access buffer for reading out one shot capture
 const uint8_t *get_sample_buffer();
 #endif
 
